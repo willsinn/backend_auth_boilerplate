c5461346bcdff88f698177d1c41c7a21af1294b1
William Sinn <w.h.sinn@gmail.com>
2019-06-17 01:57:22 -0400

    add final logic, boilerplate complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,40 +1,23 @@
-class ApplicationController < ActionController::API
-  before_action :authorized
+class Api::V1::UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
 
-  def encode_token(payload)
-    # should store secret in env variable
-    JWT.encode(payload, 'my_s3cr3t')
+  def profile
+    render json: { user: UserSerializer.new(current_user) }, status: :accepted
   end
 
-  def auth_header
-    # { Authorization: 'Bearer <token>' }
-    request.headers['Authorization']
-  end
-
-  def decoded_token
-    if auth_header
-      token = auth_header.split(' ')[1]
-      # header: { 'Authorization': 'Bearer <token>' }
-      begin
-        JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
-      rescue JWT::DecodeError
-        nil
-      end
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      @token = encode_token({ user_id: @user.id })
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
     end
   end
 
-  def current_user
-    if decoded_token
-      user_id = decoded_token[0]['user_id']
-      @user = User.find_by(id: user_id)
-    end
-  end
+  private
 
-  def logged_in?
-    !!current_user
-  end
-
-  def authorized
-    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  def user_params
+    params.require(:user).permit(:username, :password, :avatar)
   end
 end
